1c1264490d77509b6255fd070b15ed65db54c69f
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-23 15:30:36 +0500

    colorscheme changed to nightowl

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-  { "olivercederborg/poimandres.nvim" },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "poimandres",
+      colorscheme = "night-owl",
     },
   },
 }
