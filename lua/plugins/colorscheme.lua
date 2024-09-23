b8b99283572608322b8b0a76befd3667f014dc78
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-23 21:17:08 +0500

    theme updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "tokyonight",
     },
   },
 }
