f3f266331b1b433cce5384fb8231d7991ec9e581
ammarrqureshi <mamqureshi786@gmail.com>
2024-09-28 20:41:45 +0500

    lsp extras rmvd for go and ts

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,9 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
